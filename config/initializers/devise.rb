0655703ff4671f4c4339b193a3f31782484bc567
CicadaFly <110823334+CicadaFly@users.noreply.github.com>
2022-12-23 17:47:52 +0800

    2f

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,7 +25,8 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  # 修正rails 7的錯誤
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
